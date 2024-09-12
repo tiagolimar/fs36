e87782210b5ad1fd1cfcf599feef9e0f08059839
Tiago Lima <47084105+tiagolimar@users.noreply.github.com>
2024-09-12 22:47:05 +0000

    update

--- api_git/script.js
+++ api_git/script.js
@@ -12,7 +12,7 @@ const users = [
 ]
 
 async function getRepos(user){
-    const response = await fetch(`https://api.github.com/users/${user}/repos?per_pages=100`);
+    const response = await fetch(`https://api.github.com/users/${user}/repos`);
     const data = await response.json();
     // const data = [1,2,3]
     return data
@@ -38,8 +38,8 @@ async function main(){
         const {name, html_url,avatar_url} = await getUser(user);
         tbodyContent += `
         <tr>
-            <td><a href="${html_url}" target="_blank">
-            <img class="rounded rounded-5 shadow border border-primary" src="${avatar_url}" alt="logo digital college" style="height: 5em;">
+            <td><a href="${html_url}">
+            <img class="rounded rounded-5 shadow border border-primary" src="${avatar_url}" alt="logo do usuário" style="height: 5em;">
         </a></td>
             <td>${name}</td>
             <td>${user}</td>
